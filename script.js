7f069b9e5362abe36206993ba4e470d64a457aaf
beau <beau@beaus-MacBook-Pro.local>
2026-03-19 13:49:58 -0400

    small update with some bugs

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ let titleChangeInput = document.getElementById("title-change-edit");
 function showTitleChange()
 {
     changeTitleBox.style.display = 'flex';
+    hideAddTasks();
 }
 
 function hideTitleChange()
@@ -24,4 +25,80 @@ function changeTitle()
     welcomeHead.textContent = newTitle;
     titleChangeInput.value = "";
     hideTitleChange();
+}
+
+//adding tasks
+let addTaskBox = document.getElementById("tasks-box");
+let taskTask = document.getElementById("task-task");
+let taskClass = document.getElementById("task-class");
+let taskDueDate = document.getElementById("task-due-date");
+
+function hideAddTasks(){
+    addTaskBox.style.display = 'none';
+}
+
+function showAddTasks()
+{
+    addTaskBox.style.display = 'flex';
+    hideTitleChange();
+}
+
+function createDiv()
+{
+    var div = document.createElement("div");
+    var h = document.createElement("h4");
+    var p1 = document.createElement("p");
+    var p2 = document.createElement("p");
+    var button = document.createElement("button");
+
+    var addTask = taskTask.value;
+    var addClass = taskClass.value;
+    var addDueDate = taskDueDate.value;
+
+    h.textContent = addTask;
+    p1.textContent = addClass
+    p2.textContent = addDueDate;
+    button.textContent = " ";
+
+    div.style.backgroundColor = "white";
+    div.style.maxWidth = "80%";
+    div.style.margin = "10px auto";
+    div.style.borderRadius = "20px";
+    div.style.border = "3px solid black";
+    div.style.padding = "6px";
+    div.style.paddingLeft = "20px";
+    div.style.fontFamily = "Header Font";
+    div.style.position = "relative";
+
+    h.style.fontSize = "25px";
+    h.style.fontWeight = "bold";
+    h.style.marginTop = "5px";
+    h.style.marginBottom = "1px";
+
+    p1.style.marginTop = "5px";
+    p1.style.marginBottom = "5px";
+
+    p2.style.marginTop = "5px";
+    p2.style.marginBottom = "5px";
+
+    button.style.padding = "10px";
+    button.style.backgroundColor = "white";
+    button.style.border = "3px solid darkgreen";
+    button.style.top = 0;
+    button.style.right = 0;
+    button.style.marginTop = "10px";
+    button.style.marginRight = "10px";
+    button.style.borderRadius = "10px";
+    button.style.position = "absolute";
+
+    div.appendChild(h);
+    div.appendChild(p1);
+    div.appendChild(p2);
+    div.append(button);
+    document.getElementById("new-tasks").appendChild(div);
+
+    hideAddTasks();
+    taskTask.value = "";
+    taskClass.value = "";
+    taskDueDate.value="";
 }
